c1ec21e375eff9251e822e6cc721b4a2f729be35
Hector Toro <alejotoro94@hotmail.com>
2021-09-20 17:48:06 -0500

    se instala bootstrap y se inicial el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AdidasComponent } from './components/adidas/adidas.component';
+import { NikeComponent } from './components/nike/nike.component';
+const routes: Routes = [
+    { path: 'adidas', component: AdidasComponent },
+    { path: 'nike', component: NikeComponent },
+    /* { path: 'about', component: HomeComponent }, */
+    { path: '**', pathMatch: 'full', redirectTo: 'adidas' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AdidasComponent } from './components/adidas/adidas.component';
+import { NikeComponent } from './components/nike/nike.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AdidasComponent,
+    NikeComponent
   ],
   imports: [
     BrowserModule,
